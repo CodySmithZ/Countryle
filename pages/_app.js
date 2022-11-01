b0d40a89918725afcc5b0b03c4e4831f6d1bdfc0
CodySmithZ <csmit444@gmail.com>
2022-11-01 19:06:34 +1300

    Added new favicons for different devices

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import Head from "next/head";
 import { store, persistor } from "../store/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
@@ -7,6 +8,34 @@ function MyApp({ Component, pageProps }) {
 	return (
 		<Provider store={store}>
 			<PersistGate loading={null} persistor={persistor}>
+				<Head>
+					<title>Countryle - Guess the country</title>
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/favicon-16x16.png"
+					/>
+					<link rel="manifest" href="/site.webmanifest" />
+					<link
+						rel="mask-icon"
+						href="/safari-pinned-tab.svg"
+						color="#5bbad5"
+					/>
+					<meta name="msapplication-TileColor" content="#2d89ef" />
+					<meta name="theme-color" content="#ffffff" />
+				</Head>
 				<Component {...pageProps} />
 			</PersistGate>
 		</Provider>
